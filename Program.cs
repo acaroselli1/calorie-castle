955efa5df006023951beaa4f53aa202a4f95730a
Alex Caroselli <alexandersylvia9@msn.com>
2017-09-23 18:11:38 -0600

    meets basic requirements plus 3/4 bonus

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace CastleGrimtol
             while (playing)
             {
                 Console.WriteLine("");
-                Console.WriteLine("What would you like to do next? (Note: You can not use items you have not taken.)");
+                Console.WriteLine("What would you like to do next? (Note: You can not use food items you have not taken.)");
                 PlayerChoice = Console.ReadLine();
                 validateChoice(PlayerChoice);
             }
@@ -26,7 +26,7 @@ namespace CastleGrimtol
             void validateChoice(string playerchoice)
             {
 
-                if (playerchoice == "go E" || playerchoice == "go W" || playerchoice == "U" || playerchoice == "T twinkie" || playerchoice == "T oreo" || playerchoice == "T rocky road" || playerchoice == "T butterfinger" || playerchoice == "I" || playerchoice == "Q" || playerchoice == "H" || playerchoice == "U twinkie" || playerchoice == "U oreo" || playerchoice == "U butterfinger" || playerchoice == "U rocky road")
+                if (playerchoice == "e" || playerchoice == "w" || playerchoice == "U" || playerchoice == "T twinkie box" || playerchoice == "T oreo box" || playerchoice == "T icecream sandwich box" || playerchoice == "T butterfinger box" || playerchoice == "I" || playerchoice == "Q" || playerchoice == "H" || playerchoice == "U twinkie box" || playerchoice == "U oreo box" || playerchoice == "U butterfinger box" || playerchoice == "U icecream sandwich box"|| playerchoice == "U switch")
                 {
 
                     Play(playerchoice);
@@ -76,35 +76,50 @@ namespace CastleGrimtol
                     case "Q":
                         Environment.Exit(0);
                         break;
-                    case "T twinkie":
-                        Game.TakeItem("twinkie");
+                    case "T twinkie box":
+                        Game.TakeItem("twinkie box");
                         break;
-                    case "T oreo":
-                        Game.TakeItem("oreo");
+                    case "T oreo box":
+                        Game.TakeItem("oreo box");
                         break;
-                    case "T butterfinger":
-                        Game.TakeItem("butterfinger");
+                    case "T butterfinger box":
+                        Game.TakeItem("butterfinger box");
                         break;
-                    case "T rocky road":
-                        Game.TakeItem("rocky road");
+                    case "T icecream sandwich box":
+                        Game.TakeItem("icecream sandwich box");
                         break;
-                    case "U twinkie":
-                        Game.UseItem("twinkie");
+                   case "U twinkie box":
+                        Game.UseItem("twinkie box");
                         break;
-                    case "U oreo":
-                        Game.UseItem("oreo");
+                    case "U oreo box":
+                        Game.UseItem("oreo box");
                         break;
-                    case "U butterfinger":
-                        Game.UseItem("butterfinger");
+                    case "U butterfinger box":
+                        Game.UseItem("butterfinger box");
                         break;
-                    case "U rocky road":
-                        Game.UseItem("rocky road");
+                    case "U icecream sandwich box":
+                        Game.UseItem("icecream sandwich box");
+                        break; 
+                    case "U switch":
+                        Game.UseItem("switch");
+                        break;    
+                    // case "U fridge1":
+                    //     Game.UseItem("fridge1");
+                    //     break;
+                    // case "U fridge2":
+                    //     Game.UseItem("fridge2");
+                    //     break;
+                    // case "U freezerchest":
+                    //     Game.UseItem("freezerchest");
+                    //     break;
+                    // case "U freezer":
+                    //     Game.UseItem("freezer");
+                    //     break;
+                    case "e":
+                        Game.Go("e");
                         break;
-                    case "go E":
-                        Game.Go("E");
-                        break;
-                    case "go W":
-                        Game.Go("W");
+                    case "w":
+                        Game.Go("w");
                         break;
 
                 }
--- Project/Game.cs
+++ Project/Game.cs
@@ -16,8 +16,8 @@ namespace CastleGrimtol.Project
             Console.WriteLine("");
             Console.WriteLine("Game Commands");
             Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Go East or West - type 'go E' or 'go W'.--> This command moves your player between rooms.");
-            Console.WriteLine("Use item - type 'U <item>'.-->   This command uses an item from your inventory or uses an item in the room.");
+            Console.WriteLine("Go East or West - type 'e' or 'w'.--> This command moves your player between rooms.");
+            Console.WriteLine("Use item - type 'U <item>'.-->   This command uses an item from your inventory or an item bound to the room.");
             Console.WriteLine("Take item - type 'T <item>'.-->  If an item can be picked up, this command will put the item in the player inventory.");
             Console.WriteLine("Inventory - type 'I'.-->  This command lists the current items in the player inventory.");
             Console.WriteLine("Quit game - type 'Q'.-->  This command exits the game.");
@@ -50,47 +50,54 @@ namespace CastleGrimtol.Project
 
             HelpGuide();
 
-            Room Cake = new Room("Twinkie Room", "You find yourself in a room full of Twinkies, one of your favorites but sure to " +
+            Room Cake = new Room("Twinkie Room", "You find yourself in a room full of Twinkies; one of your favorites but sure to " +
             "ruin your diet! You look around the room. Boxes of Twinkies line the walls from floor to ceiling.  You are very hungry and " +
-            "the sights around you are making you want to start your diet next week.  You notice a small fridge.");
+            "the sights around you are making you want to start your diet next week.");
 
             Room Cookies = new Room("Oreo Room", "You find yourself in a room full of oreos of every variety!  You are trying to watch your " +
-            " eating habits but you never passed up an oreo before.  Boxes of Oreos line the walls from floor to ceiling.  You  are very hungry and " +
-            "the sights are making you think about starting your diet in a day or two.  You notice a small fridge.");
+            "eating habits but you never passed up an oreo before.  Boxes of Oreos line the walls from floor to ceiling.  You  are very hungry and " +
+            "the sights are making you think about starting your diet in a day or two.");
 
             Room Candy = new Room("ButterFinger Room", "Cavities and calories all in one.  You find yourself in a room full of Butterfingers. " +
             "Boxes of butterfingers line the walls from floor to ceiling.  You are trying to stay trim but you wondering if maybe your scale " +
-            "is off by 25-30 pounds.  You notice a small freezer.");
+            "is off by 25-30 pounds.");
 
-            Room Icecream = new Room("Ice-Cream Room", "A little bit of calcium and a whole lot of empty caloires. You find yourself in a freezer. " +
-            "Full of cartons of Rocky Road ice-cream.  You not one to let something like this go to waste.  Though you understand that if you don't, it " +
-            "will go to your waist.  You notice a freezer chest.");
+            Room Icecream = new Room("Ice-Cream Room", "You find yourself in a freezer full of boxes of icecream sandwiches. A little bit of calcium and a whole lot of empty calories.  You're not one to let treats like this go to waste; though you understand that, if you don't, it will go to your waist.");
+            
+            Room Win = new Room("Winner's Room!!!!", "You successfully made it through all the challenges without consuming any of the foods on the unhealthy list!! Way to go to!! You are sure to make weight for the Johnny's Fitclub Challenge and win the grand prize!!!");
 
-            Cake.Exits.Add("E", Cookies);
-            Cookies.Exits.Add("W", Cake);
-            Cookies.Exits.Add("E", Candy);
-            Candy.Exits.Add("W", Cookies);
-            Candy.Exits.Add("E", Icecream);
-            Icecream.Exits.Add("W", Candy);
+            Room Outside = new Room("Outside", "Well, you're back outside.  Go 'e' to play.");
 
-            Item Twinkie = new Item("twinkie", "Golden, delicious, but you must resist eating them!");
-            Item Oreo = new Item("oreo", "Plenty to go around with a fridge full of milk!");
-            Item Butterfinger = new Item("butterfinger", "Sweet and salty but this is not on the healthy list!");
-            Item RockyRoad = new Item("rocky road", "Don't make a rocky start on the road to wellness!");
-            Item RefrigeratorVeg = new Item("fridge", "Inside you find a bag of baby carrots and sugar snap peas.");
-            Item Freezer = new Item("freezer", "Inside you find partially frozen Snickers bars just how you like them.");
-            Item RefrigeratorMilk = new Item("fridge", "A jug of cold and delicious milk; the perfect complement to an oreo or two.");
-            Item FreezerChest = new Item("freezerchest", "You open the lid and find a medium-sized package of delicious frozen " +
-            "mango. You eat some and fill your rumbling belly. ");
+            Outside.Exits.Add("e",Cake);
+            Cake.Exits.Add("w",Outside);
+            Cake.Exits.Add("e", Cookies);
+            Cookies.Exits.Add("w", Cake);
+            Cookies.Exits.Add("e", Candy);
+            Candy.Exits.Add("w", Cookies);
+            Candy.Exits.Add("e", Icecream);
+            Icecream.Exits.Add("w", Candy);
+            Icecream.Exits.Add("e", Win);
+          
+            Item Twinkie = new Item("twinkie box", "Golden, delicious, but you must resist eating them!");
+            Item Oreo = new Item("oreo box", "Plenty to go around with a fridge full of milk!");
+            Item Butterfinger = new Item("butterfinger box", "Sweet and salty but this is not on the healthy list!");
+            Item IcecreamSandwich = new Item("icecream sandwich box", "Don't make a rocky start on the road to wellness!");
+            Item Switch = new Item("switch", "Here's a switch with an unknown function");
+            // Item RefrigeratorVeg = new Item("fridge1", "Inside you find a bag of baby carrots and sugar snap peas and satisfy your hunger with something healthy.");
+            // Item Freezer = new Item("freezer", "Inside you find partially frozen Snickers bars just how you like them.");
+            // Item RefrigeratorMilk = new Item("fridge2", "A jug of cold and delicious milk; the perfect complement to an oreo or two.");
+            // Item FreezerChest = new Item("freezerchest", "You open the lid and find a medium-sized package of delicious frozen " +
+            // "mango. You eat some and fill your rumbling belly. ");
 
             Cake.Items.Add(Twinkie);
-            Cake.Items.Add(RefrigeratorVeg);
+            // Cake.Items.Add(RefrigeratorVeg);
             Cookies.Items.Add(Oreo);
-            Cookies.Items.Add(RefrigeratorMilk);
+            // Cookies.Items.Add(RefrigeratorMilk);
             Candy.Items.Add(Butterfinger);
-            Candy.Items.Add(Freezer);
-            Icecream.Items.Add(RockyRoad);
-            Icecream.Items.Add(FreezerChest);
+            // Candy.Items.Add(Freezer);
+            Icecream.Items.Add(IcecreamSandwich);
+            Icecream.Items.Add(Switch);
+            // Icecream.Items.Add(FreezerChest);
 
             Player Alex = new Player("Alex", 100);
 
@@ -126,10 +133,19 @@ namespace CastleGrimtol.Project
         }
 
         public void Go(string direction)
-        {
+        {   
+            Console.Clear();
             CurrentRoom = CurrentRoom.Exits[direction];
             Look();
+            if (CurrentRoom.Name == "Winner's Room!!!!")
+            {
+               CurrentPlayer.Score = CurrentPlayer.Score + 100;
+               Console.WriteLine("+100pts:  You have doubled your initial score and now have the winning total of " + CurrentPlayer.Score + " points!");
+               Console.WriteLine("");
+               Environment.Exit(0);
 
+
+            }
         }
         public void Reset()
         {
@@ -137,8 +153,17 @@ namespace CastleGrimtol.Project
         //No need to Pass a room since Items can only be used in the CurrentRoom
         public void UseItem(string item)
         {
-            
-            
+            if (item == "switch" && CurrentRoom.Name == "Ice-Cream Room" )
+            {
+            Console.WriteLine("");
+            Console.WriteLine("You flip the switch not knowing its purpose.  Tired from your journey you lay down to take a short nap with freezer temperature becoming surprisingingly comfortable.  Twelve hours later you wake up dripping with sweat; feeling the affects of the summer heat wave!  You notice that the freezer once full of ice cream sandwiches is now nothing more than an ice cream pond.  The switch you flipped turned off the freezer and you're actually bathing in liquid ice cream!  The ice cream sandwich boxes are now gone from the room inventory so these are no longer an item you can take or use.");
+
+            CurrentRoom.Items.Remove(CurrentRoom.Items[0]);
+
+           
+            }
+            else
+            {
             for (var i = 0; i < CurrentPlayer.Inventory.Count; i++)
                 if (CurrentPlayer.Inventory[i].Name == item)
                 {
@@ -147,16 +172,26 @@ namespace CastleGrimtol.Project
                     if (CurrentPlayer.Score <= 0)
                     {
                         Console.WriteLine("");
-                        Console.WriteLine("-100pts:  You started by eating one and then ate 12.  Sorry, you're probably not going to make weight for the Johnny's Fitclub Challenge tomorrow!  Game Over!");
+                        Console.WriteLine("-100pts:  You start by eating one and then eat 12.  Sorry, you're probably not going to make weight for the Johnny's Fitclub Challenge weigh-in tomorrow!  Game Over!");
+                        Console.WriteLine("");
                         Environment.Exit(0);
                     }
 
                 }
-                // else if (CurrentPlayer.Inventory[i].Name != item || CurrentPlayer.Inventory.Count ==0)
-                // {
-                //     Console.WriteLine("");
-                //     Console.WriteLine("<--Sorry, you can not use an item you have not yet taken.-->");
-                // }
+            }    
+            //    for (var i = 0; i < CurrentRoom.Items.Count; i++)  
+            //      if (CurrentRoom.Items[i].Name == item)
+            //     {
+            //         CurrentPlayer.Score = CurrentPlayer.Score +25;
+
+            //         if (CurrentPlayer.Score == 100)
+            //         {
+            //             Console.WriteLine("");
+            //             Console.WriteLine("Good job!  You made all the good food choices and are on your way to wellness!  You are sure to make weight for the Johnny FitClub Challenge tomorrow!  You Win!");
+            //             Environment.Exit(0);
+            //         }
+
+            //     }
 
         }
 
@@ -165,11 +200,9 @@ namespace CastleGrimtol.Project
             for (var i = 0; i < CurrentRoom.Items.Count; i++)
             {
 
-                 System.Console.WriteLine(CurrentRoom.Items[i].Name);                 
-                 Console.WriteLine(item);
                 if (CurrentRoom.Items[i].Name == item)
                 {   
-                    Console.WriteLine(CurrentRoom.Items[i]);
+                    // Console.WriteLine(CurrentRoom.Items[i]);
                     CurrentPlayer.Inventory.Add(CurrentRoom.Items[i]);
                     CurrentRoom.Items.Remove(CurrentRoom.Items[i]);
 
